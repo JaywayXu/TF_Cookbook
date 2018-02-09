ed9b777463e372a6c4c5c6084f6d7c153c52f85c
Cloud_Xudell <1786546913@qq.com>
2018-02-09 21:35:08 +0800

    06_07

--- 06_Neural_Networks/07_Improving_Linear_Regression/07_improving_linear_regression.py
+++ 06_Neural_Networks/07_Improving_Linear_Regression/07_improving_linear_regression.py
@@ -1,5 +1,5 @@
 # Improving Linear Regression with Neural Networks (Logistic Regression)
-#----------------------------------
+# ----------------------------------
 #
 # This function shows how to use TensorFlow to
 # solve logistic regression with a multiple layer neural network
@@ -24,31 +24,55 @@ ops.reset_default_graph()
 birth_weight_file = 'birth_weight.csv'
 
 # download data and create data file if file does not exist in current directory
+# 如果当前文件夹下没有birth_weight.csv数据集则下载dat文件并生成csv文件
 if not os.path.exists(birth_weight_file):
     birthdata_url = 'https://github.com/nfmcclure/tensorflow_cookbook/raw/master/01_Introduction/07_Working_with_Data_Sources/birthweight_data/birthweight.dat'
     birth_file = requests.get(birthdata_url)
     birth_data = birth_file.text.split('\r\n')
+    # split分割函数,以一行作为分割函数，windows中换行符号为'\r\n',每一行后面都有一个'\r\n'符号。
     birth_header = birth_data[0].split('\t')
-    birth_data = [[float(x) for x in y.split('\t') if len(x)>=1] for y in birth_data[1:] if len(y)>=1]
-    with open(birth_weight_file, "w") as f:
+    # 每一列的标题，标在第一行，即是birth_data的第一个数据。并使用制表符作为划分。
+    birth_data = [[float(x) for x in y.split('\t') if len(x) >= 1] for y in birth_data[1:] if len(y) >= 1]
+    # 数组第一维表示遍历行从第一行开始，所以不包含标题，数组第二维遍历列(使用制表符进行分割)
+    # print(np.array(birth_data).shape)
+    # (189, 9)不包含标题
+    # 此为list数据形式不是numpy数组不能使用np,shape函数,但是我们可以使用np.array函数将list对象转化为numpy数组后使用shape属性进行查看。
+    # 注意，向其中写入文件时一定要去掉换行等操作符号，如果在csv中有换行符，也会作为一行数据的。
+    # 读文件时，我们把csv文件读入列表中，写文件时会把列表中的元素写入到csv文件中。
+    #
+    # list = ['1', '2', '3', '4']
+    # out = open(outfile, 'w')
+    # csv_writer = csv.writer(out)
+    # csv_writer.writerow(list)
+    # 可能遇到的问题：直接使用这种写法会导致文件每一行后面会多一个空行。
+    #
+    # 解决办法如下：
+    #
+    # out = open(outfile, 'w', newline='')  注意newline属性
+    # csv_writer = csv.writer(out, dialect='excel')
+    # csv_writer.writerow(list)
+
+    with open(birth_weight_file, "w", newline='') as f:
+        # 创建当前目录下birth_weight.csv文件
         writer = csv.writer(f)
+        writer.writerows([birth_header])
         writer.writerows(birth_data)
         f.close()
-
 # read birth weight data into memory
 birth_data = []
 with open(birth_weight_file, newline='') as csvfile:
-     csv_reader = csv.reader(csvfile)
-     birth_header = next(csv_reader)
-     for row in csv_reader:
-         birth_data.append(row)
+    csv_reader = csv.reader(csvfile)
+    birth_header = next(csv_reader)
+    for row in csv_reader:
+        birth_data.append(row)
 
 birth_data = [[float(x) for x in row] for row in birth_data]
 
 # Pull out target variable
-y_vals = np.array([x[1] for x in birth_data])
+y_vals = np.array([x[0] for x in birth_data])
 # Pull out predictor variables (not id, not target, and not birthweight)
-x_vals = np.array([x[2:9] for x in birth_data])
+x_vals = np.array([x[1:8] for x in birth_data])
+
 
 # set for reproducible results
 seed = 99
@@ -66,15 +90,16 @@ x_vals_test = x_vals[test_indices]
 y_vals_train = y_vals[train_indices]
 y_vals_test = y_vals[test_indices]
 
+
 # Normalize by column (min-max norm)
 def normalize_cols(m):
     col_max = m.max(axis=0)
     col_min = m.min(axis=0)
-    return (m-col_min) / (col_max - col_min)
-    
+    return (m - col_min)/(col_max - col_min)
+
+
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
 x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
-
 
 # Create graph
 sess = tf.Session()
@@ -86,40 +111,38 @@ y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
 # Create variable definition
 def init_variable(shape):
-    return(tf.Variable(tf.random_normal(shape=shape)))
+    return (tf.Variable(tf.random_normal(shape=shape)))
 
 
-# Create a logistic layer definition
-def logistic(input_layer, multiplication_weight, bias_weight, activation = True):
+# activation 标志位Ture则使用非线性函数sigmoid，否则使用线性函数方式
+def logistic(input_layer, multiplication_weight, bias_weight, activation=True):
     linear_layer = tf.add(tf.matmul(input_layer, multiplication_weight), bias_weight)
-    # We separate the activation at the end because the loss function will
-    # implement the last sigmoid necessary
     if activation:
-        return(tf.nn.sigmoid(linear_layer))
+        return (tf.nn.sigmoid(linear_layer))
     else:
-        return(linear_layer)
+        return (linear_layer)
 
 
-# First logistic layer (7 inputs to 7 hidden nodes)
-A1 = init_variable(shape=[7,14])
+# First logistic layer 14个神经元
+A1 = init_variable(shape=[7, 14])
 b1 = init_variable(shape=[14])
 logistic_layer1 = logistic(x_data, A1, b1)
 
-# Second logistic layer (7 hidden inputs to 5 hidden nodes)
-A2 = init_variable(shape=[14,5])
+# Second logistic layer 5个神经元
+A2 = init_variable(shape=[14, 5])
 b2 = init_variable(shape=[5])
 logistic_layer2 = logistic(logistic_layer1, A2, b2)
 
-# Final output layer (5 hidden nodes to 1 output)
-A3 = init_variable(shape=[5,1])
+# Final output layer 1个输出神经元
+A3 = init_variable(shape=[5, 1])
 b3 = init_variable(shape=[1])
 final_output = logistic(logistic_layer2, A3, b3, activation=False)
 
-# Declare loss function (Cross Entropy loss)
+# 交叉熵函数
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=final_output, labels=y_target))
 
 # Declare optimizer
-my_opt = tf.train.AdamOptimizer(learning_rate = 0.002)
+my_opt = tf.train.AdamOptimizer(learning_rate=0.002)
 train_step = my_opt.minimize(loss)
 
 # Initialize variables
@@ -135,7 +158,7 @@ accuracy = tf.reduce_mean(predictions_correct)
 loss_vec = []
 train_acc = []
 test_acc = []
-for i in range(1500):
+for i in range(2000):
     rand_index = np.random.choice(len(x_vals_train), size=batch_size)
     rand_x = x_vals_train[rand_index]
     rand_y = np.transpose([y_vals_train[rand_index]])
@@ -147,9 +170,9 @@ for i in range(1500):
     train_acc.append(temp_acc_train)
     temp_acc_test = sess.run(accuracy, feed_dict={x_data: x_vals_test, y_target: np.transpose([y_vals_test])})
     test_acc.append(temp_acc_test)
-    if (i+1)%150==0:
+    if (i + 1)%150 == 0:
         print('Loss = ' + str(temp_loss))
-        
+
 # Plot loss over time
 plt.plot(loss_vec, 'k-')
 plt.title('Cross Entropy Loss per Generation')
